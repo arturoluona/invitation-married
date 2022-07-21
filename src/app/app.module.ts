28ae826e221fb8dd8a4c18a1b57d04aa47cab6c1
Arturo <arturoluna879@gmail.com>
2022-07-20 23:24:13 -0400

    install tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { SectionFirstComponent } from './components/section-first/section-first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SectionFirstComponent
   ],
   imports: [
     BrowserModule,
