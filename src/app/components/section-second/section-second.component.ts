dcf1559ae055485c4a07e44fbc6d203b6ebad03f
Arturo Luna <arturo.luna@stockviz.com>
2023-12-08 23:47:26 +0100

    prrr

--- src/app/components/section-second/section-second.component.ts
+++ src/app/components/section-second/section-second.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-section-second',
@@ -6,25 +6,4 @@ import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
   styleUrls: ['./section-second.component.scss']
 })
 export class SectionSecondComponent {
-  @ViewChild('audioPlayer') audioPlayer: ElementRef | any;
-
-  isClick = true;
-
-  constructor(private renderer: Renderer2) {}
-
-  playAudio() {
-    try {
-      console.log(this.audioPlayer);
-      this.isClick && this.audioPlayer.nativeElement.play()
-      this.isClick = false
-    } catch {
-      this.isClick = true
-    }
-  }
-
-  ngAfterViewInit() {
-    this.renderer.listen('document', 'touchstart', () => {
-      this.playAudio();
-    });
-  }
 }
